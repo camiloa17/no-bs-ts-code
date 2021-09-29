e5bc1cbb768aee7c6cf321d49dbaae131788956d
Camilo Dominguez <cdominguez@aluna.co>
2021-09-28 17:38:02 -0700

    finish enums video

--- enums.ts
+++ enums.ts
@@ -1,15 +1,12 @@
-
-
 enum LoadingState {
   beforeLoad = 'beforeLoad',
   loading = 'loading',
   loaded = 'loaded',
 }
 
-
-const englishLoadingState={
-  [LoadingState.beforeLoad]:'Before Load'
-}
+const englishLoadingState = {
+  [LoadingState.beforeLoad]: 'Before Load',
+};
 console.log(englishLoadingState);
 const isLoading = (state: LoadingState) => state === LoadingState.loading;
 
@@ -17,3 +14,22 @@ console.log(isLoading(LoadingState.loading));
 
 //Literal types
 
+function rollDice(dice: 1 | 2 | 3): number {
+  let pip = 0;
+  for (let i = 0; i < dice; i++) {
+    pip += Math.round(Math.random() * 5 + 1);
+  }
+  return pip;
+}
+
+console.log(rollDice(3));
+
+
+function sendEvent2(name:'addToCart',data:{productId:number}):void
+function sendEvent2(name:'checkout',data:{cartCount:number}):void
+function sendEvent2(name:string,data:unknown):void{
+  console.log(`${name}: ${JSON.stringify(data)}`)
+}
+
+sendEvent2('addToCart',{productId:3})
+sendEvent2('checkout',{cartCount:4})
